f4bfce55a1ffc21a6b70e798575388c3cd185700
Gabrieluliz1 <ulizgabriel@gmail.com>
2024-05-16 13:21:53 -0300

    Update script.js

--- trabalho/script.js
+++ trabalho/script.js
@@ -1,9 +1,9 @@
 function calcularTroco() {
  
-    var valorPago = parseFloat(document.getElementById('Pagar').value);
-    var precoProduto = parseFloat(document.getElementById('Produto').value);
+    let valorPago = parseFloat(document.getElementById('Pagar').value);
+    let precoProduto = parseFloat(document.getElementById('Produto').value);
 
-    var troco = valorPago - precoProduto;
+   let troco = valorPago - precoProduto;
 
     if (troco >= 0) {
         document.getElementById('troco').innerHTML = "O seu troco R$" + troco.toFixed(2);
